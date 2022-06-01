ec769e3ec6c83dda1968b7721eac5a91d7b83a72
Jenny03b <jenniferbujalla2003@gmail.com>
2022-06-01 15:27:04 +0200

    add components

--- S6/first-project1/src/app/app.component.ts
+++ S6/first-project1/src/app/app.component.ts
@@ -3,12 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Lista Zadań';
+  config?: Foo;
 
-  get footer(): string {
-    return '2020 © Lista zadań,All rights reserved.';
+  constructor() {
+    setTimeout( () => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: '© Lista zadań zbudowana w Angularze.',
+        date: '2022',
+      };
+    }, 500);
   }
 }
+interface Foo {
+  title?: string;
+  footer?: string;
+  date?: string;
+}
+
--- S7/change-detection/src/app/app.component.ts
+++ S7/change-detection/src/app/app.component.ts
@@ -1,28 +1,39 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Change Detecion';
-  counter=0;
+  title = 'ćwiczenia';
+  num1 = 40;
+  num2 = '4';
+  falsity = false;
 
-  construction() {
-    setInterval(() => {}, 1000);
+  dog = {
+    name: 'Blacky',
+    age: 4,
+    color: 'czarny',
+  };
+
+  // pól private nie wyświetlamy, jest to pole do gettera randomBoolean
+  private booleans = [true, false];
+
+  quotation1(): string {
+    return 'Życia nie można wybrać, ale można z niego coś zrobić. - Peter Lippert';
   }
 
-  getInfo(){
-    console.log("Metoda `getInfo()` została wywołana");
-    return "Tekst wyświetlony z metdy";
+  get quotation2(): string {
+    return 'Bądź dobrej myśli, bo po co być złej. - Stanisław Lem';
   }
 
-  count() {
-    this.counter++;
+  // losuje true lub false z pola booleans
+  get randomBoolean(): boolean {
+    return this.booleans[Math.round(Math.random())];
   }
 
-  notBinded() {
-    console.log("Ta metoda nie jest bindowana");
+  divider() {
+    return 3;
   }
 }
